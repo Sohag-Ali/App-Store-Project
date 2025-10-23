bcfd9a4076ef17c434283b5e38be1dccd4fbda0c
sohag142 <sohag2879@gmail.com>
2025-10-23 21:20:59 +0600

    taxt color

--- src/Component/Pages/AppHome.jsx
+++ src/Component/Pages/AppHome.jsx
@@ -11,7 +11,7 @@ const AppHome = ({ app }) => {
             <div>
             <div className='p-3 shadow-sm rounded-lg bg-white flex flex-col justify-center items-center gap-3 overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-xl cursor-pointer'>
                 <img src={app.image} alt="" />
-                <p className='text-xl'>{app.title}</p>
+                <p className='text-xl text-[#001931] font-bold'>{app.title}</p>
                 <div className='flex justify-between gap-30'>
                     <div className="flex items-center gap-1  text-sm bg-gray-100 p-1 rounded-sm text-bold text-green-400">
                         <img className=' h-4' src={downloadImage} alt="" /> 
